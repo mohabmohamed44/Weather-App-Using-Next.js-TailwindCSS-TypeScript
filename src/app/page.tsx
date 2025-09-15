112200ccaa1a71becc46536ca14dbe95a53eb166
mohabmohamed44 <mohamedmohab742@gmail.com>
2025-09-16 01:19:44 +0300

    fix: remove debug component after fixing all errors finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -232,7 +232,7 @@ export default function Home() {
   }
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
-      <DebugInfo />
+      {/*<DebugInfo />*/}
       <Navbar location={data?.location.name} />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4 ">
         {/* today data  */}
